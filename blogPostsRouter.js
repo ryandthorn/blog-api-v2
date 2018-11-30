279101c0e0b85444f6ecb4fc4b5e710faaa8f0de
Ryan Thorn <ryandthorn@gmail.com>
2018-11-30 11:38:47 -0500

    Fix typos in server.js, export router, set GET endpoint

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -9,7 +9,11 @@ const {BlogPosts} = require('./models');
 BlogPosts.create('First Post', 'Lorem ipsum dolor', 'Author 1');
 BlogPosts.create('Second Post', 'Lorem ipsum dolor', 'Author 2');
 
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+  res.json(BlogPosts.get());
+});
 router.post('/', jsonParser, (req, res) => {});
 router.put('/:id', jsonParser, (req, res) => {});
 router.delete('/:id', (req, res) => {});
+
+module.exports = router;
