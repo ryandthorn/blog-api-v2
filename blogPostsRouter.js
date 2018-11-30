7f47447293ea4e22a873284f13f51cf99cd415f9
Ryan Thorn <ryandthorn@gmail.com>
2018-11-30 11:50:49 -0500

    Add POST endpoint

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -13,8 +13,22 @@ router.get('/', (req, res) => {
   res.json(BlogPosts.get());
 });
 
-router.post('/', jsonParser, (req, res) => {});
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!field in req.body) {
+      const message = `Missing '${field}' in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }  
+  }
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+  res.status(201).json(item);
+});
+
 router.put('/:id', jsonParser, (req, res) => {});
+
 router.delete('/:id', (req, res) => {
   BlogPosts.delete(req.params.id);
   console.log(`Deleted blog post with ID: ${req.params.ID}`);
