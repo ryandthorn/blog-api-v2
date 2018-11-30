04ae29edf19ed3329004671a78ec4a5dd5b4f77a
Ryan Thorn <ryandthorn@gmail.com>
2018-11-30 11:34:34 -0500

    Set up router framework

---
+++ blogPostsRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+BlogPosts.create('First Post', 'Lorem ipsum dolor', 'Author 1');
+BlogPosts.create('Second Post', 'Lorem ipsum dolor', 'Author 2');
+
+router.get('/', (req, res) => {});
+router.post('/', jsonParser, (req, res) => {});
+router.put('/:id', jsonParser, (req, res) => {});
+router.delete('/:id', (req, res) => {});
