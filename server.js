71a55cb191dc5fbd7354f4468bb83d9013bcc9c8
Ryan Thorn <ryandthorn@gmail.com>
2018-11-30 11:32:46 -0500

    Add router endpoint

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+const blogPostsRouter = require('/blogPosts')
 
 app.use(morgan('common'));
+
+app.use('/blog-posts', blogPostsRouter);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Blog API is listening on port ${process.env.PORT || 8080}`);
