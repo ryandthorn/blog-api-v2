78653b2a9574ad1d019bf9f7cbbf42a40614ecd1
Ryan Thorn <ryandthorn@gmail.com>
2018-12-02 09:54:19 -0500

    DELETE test passes

--- test/test-server.js
+++ test/test-server.js
@@ -72,4 +72,17 @@ describe('Blog Posts', function() {
         })
     );
   });
+  it('should delete an item on DELETE', function() {
+    return (
+      chai
+        .request(app)
+        .get('/blog-posts')
+        .then(function(res) {
+          return chai.request(app).delete(`/blog-posts/${res.body[0].id}`);
+        })
+        .then(function(res) {
+          expect(res).to.have.status(204);
+        })
+    );
+  })
 });
