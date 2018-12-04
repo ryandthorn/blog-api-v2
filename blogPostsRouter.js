a033b34d670ce591f03f6cd30b54474f6fa96194
Ryan Thorn <ryandthorn@gmail.com>
2018-12-04 10:44:50 -0500

    Add PUT and DELETE endpoints

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', false);
 
 const { BlogPosts } = require('./models');
 
@@ -51,6 +52,41 @@ router.post('/', (req, res) => {
     .catch(err => {
       console.error(err);
       res.status(500).json({ message: 'Internal server error' });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  // Check for both ids and make sure they match
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).json({message: message});
+  }
+
+  const toUpdate = {};
+  const updateableFields = ['title', 'content', 'author'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  BlogPosts
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate }, { new: true })
+    .then(post => res.status(200).json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    })
+});
+
+router.delete('/:id', (req, res) => {
+  BlogPosts
+    .findByIdAndDelete(req.params.id)
+    .then(post => res.status(204).end())
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error'});
     });
 });
 
